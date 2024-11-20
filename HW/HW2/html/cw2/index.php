9a51d29d9f0d58cab0cb4a5afafde30a03ea5161
wenyanzhang <1010756860@qq.com>
2024-11-20 12:15:03 +0800

    Second commit

---
+++ HW/HW2/html/cw2/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+    <style>
+        .card{
+            width: 500px;
+            margin: 100px auto 0;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            text-align: center;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+        }
+    </style>
+</head>
+<body>
+
+<div class="card">
+    <div>
+
+        goto to <a href="/dashboard/index.php">Dashboard</a>
+    </div>
+    <div style="font-weight: bold;color: red">
+        You can log in using an admin account or a police account.
+    </div>
+</div>
+</body>
+</html>
